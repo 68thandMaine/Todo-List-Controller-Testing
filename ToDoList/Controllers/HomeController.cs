7d9217df7e4dc73ad95a9edd86d302867aae02fb
Chris Rudnicky <chrisrudnicky@gmail.com>
2018-11-28 20:35:14 -0800

    fix homecontroller file

--- ToDoList/Controllers/HomeController.cs
+++ ToDoList/Controllers/HomeController.cs
@@ -7,24 +7,11 @@ namespace ToDoList.Controllers
     public class HomeController : Controller
     {
 
-        [Route("/items")]
+        [Route("/")]
         public ActionResult Index()
-        {
-            List<Item> allItems =Item.GetAll();
-            return View(allItems);
-        }
-
-        [Route("/items/form")]
-        public ActionResult CreateForm()
         {
             return View();
         }
-        [Route("/items/new")]
-        public ActionResult Create(string description)
-        {
-            Item myItem = new Item(description);
-            myItem.Save();
-            return RedirectToAction("Index");
         }
     }
 }
